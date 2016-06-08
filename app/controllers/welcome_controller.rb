c41d63f2b0859ae8f1254cb50e3f8a19739a5551
jorgemariscalavila <MARISCALAVILAJORGE@GMAIL.com>
2016-06-08 12:30:47 +0200

    pre-despligue en heroku

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def english
+  end
+
+  def arab
+  end
+
+  def detectlocation
+  end
+
+  def about
+  end
+
+  def us
+  end
+end
