7b6e8d96069fa0c3c7d21f550b61c8fc8b4acdbd
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2016-06-18 10:40:37 +0200

    Detect user location
    Detect user location on carted map:
    - read viz from carted map using JS
    - click on button to detect location and move map to

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,68 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(document).ready(function(){
+  $(document).foundation(); 
+  $('body').on('click', '#detector', detectUserLocation);
+});
+
+
+function notify () {
+  alert('hola');
+}
+
+
+    var map;
+    function init(){
+      // initiate leaflet map
+      map = new L.Map('map', { 
+        center: [38,24],
+        zoom: 9
+      })
+
+      L.tileLayer('https://dnv9my2eseobd.cloudfront.net/v3/cartodb.map-4xtxp73f/{z}/{x}/{y}.png', {
+        attribution: 'Mapbox <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
+      }).addTo(map);
+
+      var layerUrl = 'https://mariscalavilajorge.cartodb.com/api/v2/viz/2b0a2fc2-3179-11e6-b994-0e31c9be1b51/viz.json';
+
+
+      cartodb.createLayer(map, layerUrl)
+        .addTo(map)
+        .on('done', function(layer) {
+          // change query
+        }).on('error', function() {
+          //log the error
+        });
+    }
+
+function detectUserLocation(){
+  if (navigator.geolocation) {
+    var timeoutVal = 10 * 1000 * 1000;
+    navigator.geolocation.watchPosition(
+      mapToPosition, 
+      alertError,
+      { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+    );
+  }
+  else {
+    alert("Geolocation is not supported by this browser");
+  }
+
+  function alertError(error) {
+    var errors = { 
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+    };
+    alert("Error: " + errors[error.code]);
+  }
+}
+
+function mapToPosition(position) {
+  lon = position.coords.longitude;
+  lat = position.coords.latitude;
+  map.setView(new L.LatLng(lat,lon), 14 );
+  new L.CircleMarker([lat,lon],{radius: 4}).addTo(map);
+}
+
